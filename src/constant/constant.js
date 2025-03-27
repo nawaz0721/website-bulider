b5c3a0e98524976e94a0f3232e496062b3f1011a
Ahmed Nawaz Gondal <154045292+nawaz0721@users.noreply.github.com>
2025-03-27 23:26:01 +0500

    constant link

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,7 +1,7 @@
 //constants.js
 // http://localhost:4000/
 // https://website-bulider-backend.vercel.app/
-const devUrl = 'http://localhost:4000/';
+const devUrl = 'https://website-bulider-backend.vercel.app/';
 export const BASE_URL = devUrl
 
 export const AppRoutes = {
